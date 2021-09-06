a4dd5a655c4c126cbf292c64e327578747a6aa30
WellingtonSB <wellingtonsouza2504@gmail.com>
2021-09-06 15:43:28 -0300

    Select class

---
+++ Aulas/Ex.sql
@@ -0,0 +1,26 @@
+-- A equipe de marketing precisa fazer uma pesquisa sobre nomes mais comuns de seus clientes e precisa do nome e sobrenome de todos os  clientes que estão cadastrados no sistema;
+SELECT firstName,lastName FROM person.person
+SELECT DISTINCT firstName FROM person.person --Não retorna dados duplicados
+SELECT * FROM person.person WHERE lastName = 'miller' and firstName = 'anna'
+
+-- A equipe de produção de produtos precisa do nome de todas as peças que pesam mais que 500kg mas não mais de 700kg para inspecao
+SELECT Name FROM Production.Product WHERE weight>500 and weight<=700
+
+
+-- Foi Pedido pelo mkt de todos os empregados que são casados e são assalariados 
+SELECT * FROM HumanResouces.Employee WHERE MaritalStatus = 'm' AND SalariedFlag = 1
+
+
+-- Um usuario chamado peter krebs esta devendo um pagamento, consiga o email dele para que possamos enviar uma cobrança!
+SELECT * FROM person.person WHERE lastName = 'peter' and firstName = 'krebs'
+SELECT * FROM person.EmailAddress WHERE BusinessEntityID = 26
+
+
+
+SELECT COUNT (DISTINCT title) FROM person.person
+
+-- Há quantos produtos cadastrados na tabela (production.product)
+
+-- Qual a quantidade de tamanhos de produtos cadastrados na tabela (production.product)
+
+-- Qual a quantidade de tamanhos diferentes cadastros na tabela (production.product)
---
+++ DBA-SQL.sql
@@ -0,0 +1,13 @@
+create database firstDb; --create db
+drop database firstDb; --drop db
+
+SELECT 
+-- SQL SERVER, POSTGRES,ORACLE, MYSQL
+
+SELECT Coluna1,Coluna2 FROM tabela --find perso.
+SELECT * FROM tabela --find all
+
+
+
+
+
